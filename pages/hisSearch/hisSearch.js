b58300d913501c9fbdc776d24148e568d709fe51
薛波 <15608065769@163.com>
2020-04-07 09:28:46 +0800

    ok~

---
+++ pages/hisSearch/hisSearch.js
@@ -0,0 +1,81 @@
+var util = require("../../utils/util.js");
+Page({
+  data: {
+    date: util.dateFormat(Date.now()),
+    start: "1900-01-01",
+    end: "3000-12-31",
+    words: [],
+  },
+
+  onLoad: function () {
+    var logs = wx.getStorageSync("logs") || [];
+    this.setData({logs: logs.slice(0, 10)});
+  },
+  onShow: function () {
+    var node = this;
+    wx.request({
+      url: 'http://119.23.53.56:8080/mysql?appId=101',
+      success: function (res) {
+        if(res.data.res_code === 0) {
+          var list = [];
+          res.data.body.forEach(function (n) {
+            list.push(n.name);
+          });
+          node.setData({
+            words: list
+          })
+        }
+      }
+    });
+  },
+  onChangeDate: function (event) {
+    var value = event.detail.value;
+    this.setData({date:value});
+  },
+  changeDateByTag: function (event) {
+    var value = event.target.dataset.date;
+    this.setData({ date: value });
+  },
+  search: function () {
+    var date = this.data.date.substr(5).split('-').join('');
+    wx.showLoading({
+      title: '正在查询',
+    });
+    wx.request({
+      url: 'http://route.showapi.com/119-42',
+      data: {
+        date: date,
+        showapi_appid: "68354",
+        showapi_sign: "255dd762226b4cb689d9b5c244374e81",
+      },
+      success: function (res) {
+        var body = res.data.showapi_res_body;
+        if (body.ret_code === 0) {
+          body.list.forEach(evt => {
+            evt.date = util.compareDate(evt);
+          });
+          getApp().list = body.list;
+          wx.hideLoading();
+          wx.navigateTo({
+            url: '../history/history?date=' + date,
+          });
+        }
+        else {
+          wx.hideLoading();
+          wx.showToast({
+            title: '查询失败，' + body.error_info,
+            icon: 'none'
+          });
+        }
+      },
+      fail: function (e) {
+        console.log(e);
+        wx.hideLoading();
+        wx.showToast({
+          title: '查询失败:'+e.errMsg,
+          icon: "none"
+        })
+      }
+    });
+  }
+})
---
+++ pages/history/history.js
@@ -0,0 +1,42 @@
+var util = require('../../utils/util.js');
+Page({
+  data: {
+    date: '',
+    list: [],
+    color: [
+      {
+        index: 0,
+        color: "#000",
+        selected: false
+      },
+      {
+        index: 1,
+        color: "#f00",
+        selected: false
+      },
+      {
+        index: 2,
+        color: "#00ac00",
+        selected: false
+      },
+      {
+        index: 3,
+        color: "#ffa0c1",
+        selected: false
+      }
+    ]
+  },
+  onLoad: function (query) {
+    this.setData({
+      date: query.date,
+      list: getApp().list
+    });
+    for(var i = 0;i < this.data.list.length;i++){
+        this.data.list[i].day=i;
+        this.data.list[i].month=this.data.color[i%4].color;
+    }
+    this.setData({
+      list:this.data.list
+    });
+  },
+})
